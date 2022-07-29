855e90c96f744e063a8c5aee8d32cf5fda5ad883
ColinH58 <choolwerf@gmail.com>
2022-07-29 12:42:42 -0500

    finished up form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import React, { useState } from "react";
 import './App.css';
+import ToDoList from "./Components/ToDoList";
+import ToDoForm from "./Components/ToDoForm";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
   return (
     <div>
+      <ToDoForm setTodos={setTodos} />
+      <ToDoList todos={todos} />
     </div>
   );
 }
--- src/Components/ToDoForm.jsx
+++ src/Components/ToDoForm.jsx
@@ -1,13 +1,37 @@
-import React from 'react'
+import React, { useState } from "react";
 
-const ToDoForm = () => {
+const ToDoForm = ({ setTodos }) => {
+  const [todo, setTodo] = useState({
+    id: "",
+    task: "",
+    completed: false,
+  });
+
+  function handleInput(e) {
+    setTodo({ ...todo, entry: e.target.value });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (todo.task.trim()) {
+      setTodos({ ...todo, id: todo.task });
+      setTodo({ ...todo, task: "" });
+    }
+  }
+
   return (
     <div>
-        <form>
-
-        </form>
+      <form onSubmit={handleSubmit}>
+        <input
+          name="todo"
+          type="text"
+          value={todo.task}
+          onChange={handleInput}
+        />
+        <button type="submit">Submit</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default ToDoForm
+export default ToDoForm;
